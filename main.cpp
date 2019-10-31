716a7fefd2b60428d7720a5f1e586209a0ebbbc6
jrefsgaard <jrefsgaard@triumf.ca>
2019-10-31 12:03:04 -0700

    Added parsing of command line arguments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <string>
 #include <iostream>
 #include <memory>
 #include <cstdlib>
@@ -9,11 +11,51 @@
 #include "YY1Detector.h"
 #include "Calibrator.h"
 
-
 using namespace std;
+
+void PrintHelp(char *argv[]);
 
 int main(int argc , char *argv[])
 {
+  //Parse the input.
+  string configFile, logFile, outputFile, dataFile;
+  int c;
+  while((c = getopt(argc, argv,"c:o:l:h")) != -1){
+    switch (c){
+      case 'c':
+        configFile = optarg;
+        break;
+      case 'l':
+        logFile = optarg;
+        break;
+      case 'o':
+        outputFile = optarg;
+        break;
+      case 'h':
+        PrintHelp(argv);
+        return 0;
+      case '?':
+        PrintHelp(argv);
+        return 1;
+      default:
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  if(optind == argc-1){
+    dataFile = argv[optind];
+  }
+  else{
+    PrintHelp(argv);
+    return 1;
+  }
+  
+  cout << configFile << endl;
+  cout << logFile << endl;
+  cout << outputFile << endl;
+  cout << dataFile << endl;
+  
+  /*
   TF1 f("f","TMath::Gaus(x,1000,10)+TMath::Gaus(x,2000,15)",0,4096);
   vector<shared_ptr<TH1>> spectra;
   vector<vector<double>> energies;
@@ -31,7 +73,7 @@ int main(int argc , char *argv[])
   c.SetLogFile("test.root");
   c.GetPeakFinder().SetSigma(10);
   c.Calibrate(spectra,energies);
-  
+  */
   /*
   YY1Detector Yu1;
   TVector3 source(0.,0.,0.);
@@ -41,5 +83,19 @@ int main(int argc , char *argv[])
   }
   */
   return 0;
+}
+
+void PrintHelp(char *argv[])
+{
+  cout << "Usage: " << argv[0] << " [OPTIONS] ... [DATAFILE]" << endl;
+  cout << "Detector energy calibration based on the data in [DATAFILE]." << endl;
+  cout << endl;
+  cout << "  -c [FILE]      Load configuration from [FILE] (required)." << endl;
+  cout << "  -h             Print this message." << endl;
+  cout << "  -l [FILE]      Store logging information in [FILE] (optional)." << endl;
+  cout << "  -o [FILE]      Store calibration coefficients in [FILE] (optional)." << endl;
+  cout << endl;
+  cout << "Example:" << endl;
+  cout << argv[0] << " -c /config/file -o /output/file /data/file" << endl;
 }
   
